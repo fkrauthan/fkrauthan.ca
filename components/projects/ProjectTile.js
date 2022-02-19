62725f5fe934b720957e5771f1ff198a3d7289fd
Florian Krauthan <mail@fkrauthan.de>
2022-02-19 12:41:34 -0800

    Restyled the project page slightly

--- components/projects/ProjectTile.js
+++ components/projects/ProjectTile.js
@@ -24,7 +24,7 @@ export default function ProjectTile({
   const startYear = new Date(developmentStart).getFullYear();
 
   return (
-    <div className="block rounded-md border p-5 hover:bg-yellow-100">
+    <div className="block rounded-md border p-5 hover:bg-yellow-100 flex flex-col">
       <div className="flex justify-between mb-3">
         <div className="prose">
           <h3 style={{ marginBottom: 0 }}>
@@ -41,38 +41,41 @@ export default function ProjectTile({
         <MarkdownRenderer ast={summary} />
       </div>
 
-      <div className="mt-5">
-        <div className="flex">
-          <div className="flex-1">
-            <TechnologyItem title={baseTechnology} />
-            {technologies.map((technology) => (
-              <TechnologyItem key={technology} title={technology} />
-            ))}
-          </div>
-          <div className="flex-none self-end">
-            {website && (
-              <a
-                href={website}
-                style={{ textDecoration: "none" }}
-                className="inline-block border p border-yellow-500 rounded-full border-opacity-0 hover:border-opacity-100"
-                title={`${title} Project Website`}
-                target="_blank"
-                rel="noopener noreferrer">
-                <GlobeOutline width="32px" height="32px" />
-              </a>
-            )}
+      <div className="flex-1" />
+      <div className="mt-5 flex-none">
+        <div className="flex-none">
+          <div className="flex">
+            <div className="flex-1 self-center" style={{ minHeight: "32px" }}>
+              <TechnologyItem title={baseTechnology} />
+              {technologies.map((technology) => (
+                <TechnologyItem key={technology} title={technology} />
+              ))}
+            </div>
+            <div className="flex-none self-end">
+              {website && (
+                <a
+                  href={website}
+                  style={{ textDecoration: "none" }}
+                  className="inline-block border p border-yellow-500 rounded-full border-opacity-0 hover:border-opacity-100"
+                  title={`${title} Project Website`}
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  <GlobeOutline width="32px" height="32px" />
+                </a>
+              )}
 
-            {github && (
-              <a
-                href={github}
-                style={{ textDecoration: "none" }}
-                className="inline-block border p border-yellow-500 rounded-full border-opacity-0 hover:border-opacity-100"
-                title={`${title} GitHub`}
-                target="_blank"
-                rel="noopener noreferrer">
-                <LogoGithub width="32px" height="32px" />
-              </a>
-            )}
+              {github && (
+                <a
+                  href={github}
+                  style={{ textDecoration: "none" }}
+                  className="inline-block border p border-yellow-500 rounded-full border-opacity-0 hover:border-opacity-100"
+                  title={`${title} GitHub`}
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  <LogoGithub width="32px" height="32px" />
+                </a>
+              )}
+            </div>
           </div>
         </div>
       </div>
