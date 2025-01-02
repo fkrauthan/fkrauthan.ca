1fe6c48557e377a83d780430a925794bc336b8fa
Florian Krauthan <mail@fkrauthan.de>
2025-01-01 22:02:45 -0800

    Mark project as static export

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 module.exports = withBundleAnalyzer({
+  output: "export",
   reactStrictMode: true,
   webpack(config) {
     config.plugins.push(
